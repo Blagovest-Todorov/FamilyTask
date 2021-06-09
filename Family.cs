decf1a30f44c0c88245902a4769c5b19d0eaae91
Blagovest-Todorov <blagovest.stod@abv.bg>
2021-06-09 12:36:29 +0300

    add 3 Files-Family.cs, Person.cs, StartUp.cs

---
+++ Family.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Family
+    {
+        private readonly HashSet<Person> members; // readonly is immutalbe! 
+
+        // Good Practice! 
+        public Family()
+        { // Good Practice! 
+            this.members = new HashSet<Person>();
+        }
+
+        public void AddMember(Person member) 
+        {
+            this.members.Add(member);
+        }
+
+        public Person GetOldestMember()
+            => this.members.OrderByDescending(p => p.Age).FirstOrDefault();
+        //{
+        //Explicitely  typed
+        //Person person = this.members.OrderByDescending(p => p.Age).FirstOrDefault();
+        //return person;
+        //}
+
+        public HashSet<Person> GetPeopleAbove30()
+            => this.members.Where(p => p.Age > 30).OrderBy(p => p.Name).ToHashSet();
+    }
+}
---
+++ StartUp.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            //string name = "Pesho";
+            //int age = 24;
+
+            //Person pesho = new Person("Pesho", 24);
+            // Console.WriteLine($"{pesho.Name} - {pesho.Age}");
+            //var noName = new Person();
+            //Console.WriteLine($"NoNameGuy{noName.Name} - {noName.Age}");
+
+            //var george = new Person(24);
+            //Console.WriteLine($"GeorgeYears: {george.Name} - {george.Age}");
+
+            //var ivan = new Person("Ivan", 27);
+            //Console.WriteLine($"Ivan: {ivan.Name} - {ivan.Age}");
+
+            int n = int.Parse(Console.ReadLine());
+            var family = new Family();
+
+            for (int i = 0; i < n; i++)
+            {
+                var cmdArgs = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string name = cmdArgs[0];
+                int age = int.Parse(cmdArgs[1]);
+                var person = new Person(name, age);
+                family.AddMember(person);
+            }
+
+            HashSet<Person> filteredPersonAbove30 = family.GetPeopleAbove30();
+            Console.WriteLine(string.Join(Environment.NewLine, filteredPersonAbove30));
+        }
+    }
+}
